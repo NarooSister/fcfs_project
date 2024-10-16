ae2b7779de4e294ad74967da705b84db3763f559
NarooSister <dongeun49@naver.com>
2024-10-16 11:24:14 +0900

    Initial commit

---
+++ src/main/java/com/sparta/fcfs_project/FcfsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.fcfs_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcfsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcfsProjectApplication.class, args);
+	}
+
+}
